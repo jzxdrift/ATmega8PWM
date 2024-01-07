3ff8144b086c31175b3c95c668c8eb2843ee5e7d
Albert Muller <jzxdrift@users.noreply.github.com>
2024-01-07 14:41:42 -0800

    Update buttons.h

--- Headers/buttons.h
+++ Headers/buttons.h
@@ -3,10 +3,10 @@
 
 #include <stdint.h>
 
-#define LONG_BOUNCE					   200
-#define SHORT_BOUNCE				   35
+#define LONG_BOUNCE              200
+#define SHORT_BOUNCE              35
 
-#define NO_BUTTON					    (-1)
+#define NO_BUTTON                 (-1)
 #define DUTY_CYCLE_INC_BUTTON		  0
 #define DUTY_CYCLE_DEC_BUTTON		  1
 #define FREQUENCY_INC_BUTTON		  2
